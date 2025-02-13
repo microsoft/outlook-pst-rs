358f811de720cbce0a127cf803a211ea430afb42
Bill Avery <billav@microsoft.com>
2025-02-12 23:25:51 -0800

    feat: implement read/write NDB header support

--- crates/pst/src/lib.rs
+++ crates/pst/src/lib.rs
@@ -1,5 +1,22 @@
 #![doc = include_str!("../README.md")]
 
+use std::{fs::File, io, path::Path};
+
 pub mod ltp;
 pub mod messaging;
 pub mod ndb;
+
+mod crc;
+
+/// [PST File](https://learn.microsoft.com/en-us/openspecs/office_file_formats/ms-pst/6b57253b-0853-47bb-99bb-d4b8f78105f0)
+pub struct PstFile {
+    _file: File,
+}
+
+impl PstFile {
+    pub fn new(path: impl AsRef<Path>) -> io::Result<Self> {
+        Ok(Self {
+            _file: File::open(path)?,
+        })
+    }
+}
