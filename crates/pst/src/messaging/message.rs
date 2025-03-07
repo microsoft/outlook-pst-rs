df8cef125e6401efbd32d700c74254c41d907a1b
Bill Avery <billav@microsoft.com>
2025-03-06 18:16:11 -0800

    feat: allow filtereing property IDs when loading messages

--- crates/pst/src/messaging/message.rs
+++ crates/pst/src/messaging/message.rs
@@ -151,7 +151,11 @@ impl<'a> UnicodeMessage<'a> {
         self.store
     }
 
-    pub fn read(store: &'a UnicodeStore, entry_id: &EntryId) -> io::Result<Self> {
+    pub fn read(
+        store: &'a UnicodeStore,
+        entry_id: &EntryId,
+        prop_ids: Option<&[u16]>,
+    ) -> io::Result<Self> {
         let node_id = entry_id.node_id();
         let node_id_type = node_id.id_type()?;
         match node_id_type {
@@ -180,10 +184,14 @@ impl<'a> UnicodeMessage<'a> {
             node_btree.find_entry(file, u64::from(u32::from(node_id)))?
         };
 
-        Self::read_embedded(store, node)
+        Self::read_embedded(store, node, prop_ids)
     }
 
-    pub fn read_embedded(store: &'a UnicodeStore, node: UnicodeNodeBTreeEntry) -> io::Result<Self> {
+    pub fn read_embedded(
+        store: &'a UnicodeStore,
+        node: UnicodeNodeBTreeEntry,
+        prop_ids: Option<&[u16]>,
+    ) -> io::Result<Self> {
         let pst = store.pst();
         let header = pst.header();
         let root = header.root();
@@ -212,6 +220,7 @@ impl<'a> UnicodeMessage<'a> {
             let properties = prop_context
                 .properties(file, encoding, &block_btree)?
                 .into_iter()
+                .filter(|(prop_id, _)| prop_ids.map_or(true, |ids| ids.contains(prop_id)))
                 .map(|(prop_id, record)| {
                     prop_context
                         .read_property(file, encoding, &block_btree, record)
